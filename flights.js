0bbfcb105a0676b22bba71356244618d7e7f6c95
Mohammd Amin Nazari <mnazari@edu.hse.ru>
2017-12-11 23:07:25 +0300

    Update flights.js

--- flights.js
+++ flights.js
@@ -1,71 +1,71 @@
-require('dotenv-extended').load();
+// require('dotenv-extended').load();
 
-var builder = require('botbuilder');
-var restify = require('restify');
+// var builder = require('botbuilder');
+// var restify = require('restify');
 
-var FacebookDataModels = require('./facebook-channeldata');
+// var FacebookDataModels = require('./facebook-channeldata');
 
 module.exports = function (session) {
-    //session.error('Flights Dialog is not implemented and is instead being used to show Bot error handling');
+    session.error('Flights Dialog is not implemented and is instead being used to show Bot error handling');
     
-    session.send('Looking into your upcoming flights to see if you can check-in on any of those...');
+//     session.send('Looking into your upcoming flights to see if you can check-in on any of those...');
 
-    var now = new Date();
+//     var now = new Date();
 
-    // Airline Checkin
-    var checkin = new FacebookDataModels.AirlineCheckin(
-        'Check-in is available now',
-        'en_US',
-        'ABCDED',
-        'http://www.airline.com/check_in',
-        [
-            new FacebookDataModels.FlightInfo(
-                'F001',
-                new FacebookDataModels.Airport(
-                    'SFO',
-                    'San Francisco',
-                    'T4',
-                    'G8'
-                ),
-                new FacebookDataModels.Airport(
-                    'EZE',
-                    'Buenos Aires',
-                    'C',
-                    'A2'
-                ),
-                new FacebookDataModels.FlightSchedule(
-                    now.addDays(1),
-                    now.addDays(1).addHours(1.5),
-                    now.addDays(2)
-                ))
-        ]);
+//     // Airline Checkin
+//     var checkin = new FacebookDataModels.AirlineCheckin(
+//         'Check-in is available now',
+//         'en_US',
+//         'ABCDED',
+//         'http://www.airline.com/check_in',
+//         [
+//             new FacebookDataModels.FlightInfo(
+//                 'F001',
+//                 new FacebookDataModels.Airport(
+//                     'SFO',
+//                     'San Francisco',
+//                     'T4',
+//                     'G8'
+//                 ),
+//                 new FacebookDataModels.Airport(
+//                     'EZE',
+//                     'Buenos Aires',
+//                     'C',
+//                     'A2'
+//                 ),
+//                 new FacebookDataModels.FlightSchedule(
+//                     now.addDays(1),
+//                     now.addDays(1).addHours(1.5),
+//                     now.addDays(2)
+//                 ))
+//         ]);
     
-    var reply = new builder.Message(session);
-    if (session.message.address.channelId === 'facebook') {
-        reply.sourceEvent({
-            facebook: {
-                attachment: checkin
-            }
-        });
-    } else {
-        reply.text(checkin.toString());
-    }
+//     var reply = new builder.Message(session);
+//     if (session.message.address.channelId === 'facebook') {
+//         reply.sourceEvent({
+//             facebook: {
+//                 attachment: checkin
+//             }
+//         });
+//     } else {
+//         reply.text(checkin.toString());
+//     }
 
-    session.send(reply);
+//     session.send(reply);
 };
 // Helpers
-Date.prototype.addDays = function (days) {
-    var date = new Date(this.valueOf());
-    date.setDate(date.getDate() + days);
-    return date;
-};
+// Date.prototype.addDays = function (days) {
+//     var date = new Date(this.valueOf());
+//     date.setDate(date.getDate() + days);
+//     return date;
+// };
 
-Date.prototype.addHours = function (hours) {
-    var date = new Date(this.valueOf());
-    date.setTime(date.getTime() + hours * 60 * 60 * 1000);
-    return date;
-};
+// Date.prototype.addHours = function (hours) {
+//     var date = new Date(this.valueOf());
+//     date.setTime(date.getTime() + hours * 60 * 60 * 1000);
+//     return date;
+// };
 
-function formatDate(date) {
-    return date.toISOString().split('.')[0];
-}
+// function formatDate(date) {
+//     return date.toISOString().split('.')[0];
+// }
